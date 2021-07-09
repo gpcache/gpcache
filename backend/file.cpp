2005147855787f55e3a382ae62a244d382097952
Alexander Lanin <alex@lanin.de>
2021-07-09 20:58:31 +0200

    start on file cache

--- backend/file.cpp
+++ backend/file.cpp
@@ -1,6 +1,10 @@
 #include "backend/file.h"
 #include "wrappers/hash.h"
 
+#include <fstream>
+#include <string>
+#include <cerrno>
+
 namespace gpcache
 {
   static auto make_safe_filename(const std::string_view input) -> std::string
@@ -11,6 +15,26 @@ namespace gpcache
   auto ensure_json_content(const std::filesystem::path &file, const json &content) -> bool
   {
     return true;
+  }
+
+  auto read_file(const std::filesystem::path &path) -> std::variant<std::string, int>
+  {
+    // todo: exact effect of binary
+    std::ifstream in(path, std::ios::in | std::ios::binary);
+    if (!in)
+    {
+      return errno;
+    }
+    else
+    {
+      std::string content;
+      in.seekg(0, std::ios::end);
+      content.resize(in.tellg());
+      in.seekg(0, std::ios::beg);
+      in.read(&content[0], content.size());
+      in.close();
+      return content;
+    }
   }
 
   auto cache_input(const std::filesystem::path &path, const std::string &input_name, const json &action, const json &result)
@@ -25,6 +49,15 @@ namespace gpcache
     const auto action_file = path / "action.txt";
     const auto result_file = result_path / "readable_result_for_debugging.txt";
 
+    std::filesystem::create_directories(path);
+    if (std::filesystem::exists(action_file))
+    {
+      auto res = read_file(action_file);
+      if (const auto existing_file_content = std::get_if<std::string>(&res))
+      {
+        fmt::print("Existing action_file {} = {}", action_file, existing_file_content);
+      }
+    }
     //if(!path_exists(path))
     //create_directory(path);
     //if(file_exists) assert content is action_file_content
@@ -35,6 +68,7 @@ namespace gpcache
     return result_path;
   }
 
+  // traverse_inputs?
   auto create_output_path(const Inputs inputs)
   {
     fmt::print("\n");
